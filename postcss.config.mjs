dece9f1bdd90c21e6813e89d9d885d36926d4385
ISAMU0805 <maeda136isamu@icloud.com>
2025-12-23 23:34:15 +0900

    Fix postcss config for tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},  // ← ★ここを変更しました
     autoprefixer: {},
   },
 };
