89370b0ac2850ab31d8db499862f34fccd48639a
ISAMU0805 <maeda136isamu@icloud.com>
2025-12-23 23:52:16 +0900

    Reset tailwind config to standard v3

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},  // ← ★ここを変更しました
-    autoprefixer: {},
-  },
-};
-
-export default config;
